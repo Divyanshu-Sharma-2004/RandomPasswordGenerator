ceeec54ac3189e6acfb9d42a74dfff5aff3e381d
Divyanshu-Sharma-2004 <divyanshusharma7982@gmail.com>
2024-11-26 00:17:58 +0530

    Add files via upload

---
+++ RandaomPasswordGenrator.py
@@ -0,0 +1,10 @@
+# List comprehension technique
+
+import random
+import string
+
+charValues = string.ascii_letters + string.digits + string.punctuation
+pass_len = int(input("Enter your desired password length : "))
+
+password = "".join([random.choice(charValues) for i in range(pass_len)])   # ("".join() is concatinate the list)
+print("your random password is :",password)
